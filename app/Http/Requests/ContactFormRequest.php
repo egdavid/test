5a6702d278634ea6f0da709e97142a1f7712c149
David <davz.eu@gmail.com>
2020-05-27 04:27:34 +0200

    Validation des types

--- app/Http/Requests/ContactFormRequest.php
+++ app/Http/Requests/ContactFormRequest.php
@@ -27,7 +27,7 @@ class ContactFormRequest extends FormRequest
             'name'      => 'required|min:5|max:100',
             'phone'     => 'nullable|numeric',
             'message'   => 'required|min:10',
-            'type'      => 'required'
+            'type'      => 'required|in:Recrutement,Aide,Devis'
         ];
     }
 }
